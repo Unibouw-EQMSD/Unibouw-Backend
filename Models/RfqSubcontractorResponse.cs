d69c92ae4a577381d3ccecfcba2cd2118284b5e9
Ashwinip03 <ashwini.p@flatworldsolutions.com>
2025-11-19 17:45:39 +0530

    Models commit.

---
+++ Models/RfqSubcontractorResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace UnibouwAPI.Models
+{
+    public class RfqSubcontractorResponse
+    {
+        public Guid RfqSubcontractorResponseID { get; set; }
+        public Guid RfqID { get; set; }
+        public Guid SubcontractorID { get; set; }
+        public Guid? RfqResponseID { get; set; } // Maps to Interested / Not Interested etc.
+        public bool? Viewed { get; set; }
+        public DateTime? ViewedOn { get; set; }
+        public bool? Downloaded { get; set; }
+        public DateTime? DownloadedOn { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
