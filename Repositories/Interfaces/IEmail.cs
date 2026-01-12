3d4e4a9e98ad60970cadc1d3c8c93663311637da
nitish.r <nitish.ra@flatworldsolutions.com>
2026-01-12 17:44:35 +0530

    Email Fixed

--- Repositories/Interfaces/IEmail.cs
+++ Repositories/Interfaces/IEmail.cs
@@ -7,7 +7,12 @@ namespace UnibouwAPI.Repositories.Interfaces
         //Task<bool> SendRfqEmailAsync(EmailRequest request);
         Task<List<EmailRequest>> SendRfqEmailAsync(EmailRequest request);
 
-        Task<bool> SendReminderEmailAsync(Guid subcontractorId, string email, string name, Guid rfqId, string emailBody);
+        Task<bool> SendReminderEmailAsync(
+      Guid subcontractorId,
+      string recipientEmail,
+      string subcontractorName,
+      Guid rfqId,
+      string emailBody);
 
         // Task<bool> SendMailAsync(string toEmail, string subject, string body, string name);
         Task<bool> SendMailAsync(string toEmail, string subject, string body, string name, List<string>? attachmentFilePaths = null);
